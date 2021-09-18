601b4a8a8e0ab4af0cd677650b1ebb6402338da4
Billy Everyteen <nilsoncv2@gmail.com>
2021-09-18 00:34:08 -0400

    Tela inicial do App

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { 
+  View, 
+  Text, 
+  TouchableOpacity,
+  Image,
+  StatusBar
+} from 'react-native';
+
+import { ButtonIcon } from '../../components/ButtonIcon';
+import IllustrationImg from '../../assets/illustration.png';
+import { styles } from './styles';
+
+export function SingIn() {
+
+  return(
+    <View style={styles.container}>
+      
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      <Image 
+        source={IllustrationImg} 
+        style={styles.image} 
+        resizeMode="stretch"
+      />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>          
+          Organize {`\n`} 
+          Suas Jogatinas {`\n`} 
+          facilmente
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {`\n`}
+          favoritos com seus amigos
+        </Text>
+
+        <ButtonIcon 
+          title={"Entrar com Discord"} 
+          activeOpacity={0.7}
+        />
+
+      </View>
+
+
+    </View>
+  );
+}
